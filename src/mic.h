ef30b730331e219c5884d5b03879dbfdf38cdbfc
Ray <raysan5@gmail.com>
2021-10-14 00:12:23 +0200

    Setup function definitions to fill

--- src/mic.h
+++ src/mic.h
@@ -2,7 +2,7 @@
 *
 *   mic (make-it-c) - A simple and easy-to-use library to build pipelines in C
 *
-*   NOTES: 
+*   NOTES:
 *       Memory footprint of this library is aproximately xxx bytes (global variables)
 *
 *   CONFIGURATION:
@@ -25,17 +25,17 @@
 *   LICENSE: MIT License
 *
 *   Copyright (c) 2021 Ramon Santamaria (@raysan5)
-*   
+*
 *   Permission is hereby granted, free of charge, to any person obtaining a copy
 *   of this software and associated documentation files (the "Software"), to deal
 *   in the Software without restriction, including without limitation the rights
 *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 *   copies of the Software, and to permit persons to whom the Software is
 *   furnished to do so, subject to the following conditions:
-*   
+*
 *   The above copyright notice and this permission notice shall be included in all
 *   copies or substantial portions of the Software.
-*   
+*
 *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -141,7 +141,7 @@ MICAPI void micBeginStep(const char *description, int level);           // [!] B
 MICAPI void micEndStep(void);                                           // End current step
 
 MICAPI int micExecuteCommand(const char *command, ...);                 // Execute command line command, parameters passed as additional arguments
-MICAPI int micExecuteMIC(const char *micFile);                          // Compile and execute another mic file 
+MICAPI int micExecuteMIC(const char *micFile);                          // Compile and execute another mic file
 //MICAPI void micPrintMessage(const char *message, ...);                // [!] Probably not required -> Just use printf()
 
 // Timming
@@ -268,6 +268,480 @@ MICAPI unsigned char *micDecompressData(unsigned char *compData, int compDataLen
 //----------------------------------------------------------------------------------
 // Module Functions Definition
 //----------------------------------------------------------------------------------
-//...
+
+// Log System
+//----------------------------------------------------------------------------------
+
+// Show trace log messages (LOG_DEBUG, LOG_INFO, LOG_WARNING, LOG_ERROR...)
+void micTraceLog(int logLevel, const char *text, ...)
+{
+
+}
+
+// Set the current threshold (minimum) log level
+void micSetTraceLogLevel(int logLevel)
+{
+
+}
+
+// Set custom trace log
+void micSetTraceLogCallback(micTraceLogCallback callback)
+{
+
+}
+
+// Environment
+//----------------------------------------------------------------------------------
+
+// Setup environment config flags
+void micSetEnvironmentFlags(unsigned int flags)
+{
+
+}
+
+// Set environment path (added to system PATH)
+void micSetEnvironmentPath(const char *path)
+{
+
+}
+
+// Get environment info: enum micEnvInfo
+const char *micGetEnvironmentInfo(int info)
+{
+
+}
+
+// Processes execution
+//----------------------------------------------------------------------------------
+
+// [!] Begin a new process -> Not sure yet how use this, maybe just for logging...
+void micBeginProcess(const char *description, int level)
+{
+
+}
+
+// End current process
+void micEndProcess(void)
+{
+
+}
+
+// [!] Begin a new process step -> Not sure yet how use it
+void micBeginStep(const char *description, int level)
+{
+
+}
+
+// End current step
+void micEndStep(void)
+{
+
+}
+
+// Execute command line command, parameters passed as additional arguments
+int micExecuteCommand(const char *command, ...)
+{
+
+}
+
+// Compile and execute another mic file
+int micExecuteMIC(const char *micFile)
+{
+
+}
+
+// Timming
+//----------------------------------------------------------------------------------
+// [!] Initialize internal timer -> Support multiple timers?
+void micInitTimer(void)
+{
+
+}
+
+// Get elapsed time in seconds since micInitTimer()
+double micGetTime(void)
+{
+
+}
+
+// Get current date and time (now)
+long micGetTimeStamp(void)
+{
+
+}
+
+// [!] Get timestamp as a string -> format? or just let the user manage it?
+const char *micGetTimeStampString(long timestamp)
+{
+
+}
+
+// Wait (sleep) a specific amount of time
+int micWaitTime(int milliseconds)
+{
+
+}
+
+// File system: Edition
+//----------------------------------------------------------------------------------
+
+// Create an empty file, useful for further filling
+int micMakeFile(const char *fileName)
+{
+
+}
+
+// Delete an existing file
+int micDeleteFile(const char *fileName)
+{
+
+}
+
+// Rename an existing file
+int micRenameFile(const char *fileName, const char *newFileName)
+{
+
+}
+
+// Copy an existing file to a new path and filename
+int micCopyFile(const char *srcFileName, const char *dstPathFileName)
+{
+
+}
+
+// Move an existing file to a new path and filename
+int micMoveFile(const char *srcFileName, const char *dstpathFileName)
+{
+
+}
+
+// Create an empty directory
+int micMakeDirectory(const char *dirPathName)
+{
+
+}
+
+// Delete an existing and empty directory
+int micDeleteDirectory(const char *dirPath)
+{
+
+}
+
+// Rename an existing directory
+int micRenameDirectory(const char *dirPath)
+{
+
+}
+
+// Copy an existing directory to a new path
+int micCopyDirectory(const char *srcDirPath, const char *dstDirPath)
+{
+
+}
+
+// Move an existing directory to a new path
+int micMoveDirectory(const char *srcDirPath, const char *dstDirPath)
+{
+
+}
+
+// File system: Query
+//----------------------------------------------------------------------------------
+
+// Check if a file exists
+bool micIsFileAvailable(const char *fileName)
+{
+
+}
+
+// Check if a directory path exists
+bool micIsDirectoryAvailable(const char *dirPath)
+{
+
+}
+
+// Get current working directory (uses static string)
+const char *micGetWorkingDirectory(void)
+{
+
+}
+
+// Change working directory, return true on success
+bool micChangeDirectory(const char *dirPath)
+{
+
+}
+
+// Get file size
+int micGetFileSize(const char *fileName)
+{
+
+}
+
+// Get file modification time (last write time)
+long micGetFileModTime(const char *fileName)
+{
+
+}
+
+// Check file extension (including point: .png, .wav)
+bool micIsFileExtension(const char *fileName, const char *ext)
+{
+
+}
+
+// Get pointer to extension for a filename string (includes dot: '.png')
+const char *micGetFileExtension(const char *fileName)
+{
+
+}
+
+// Get pointer to filename for a path string
+const char *micGetFileName(const char *filePath)
+{
+
+}
+
+// Get filename string without extension (uses static string)
+const char *micGetFileNameWithoutExt(const char *filePath)
+{
+
+}
+
+// Get full path for a given fileName with path (uses static string)
+const char *micGetDirectoryPath(const char *filePath)
+{
+
+}
+
+// Get previous directory path for a given path (uses static string)
+const char *micGetPrevDirectoryPath(const char *dirPath)
+{
+
+}
+
+// Get full path for a file
+const char *micGetFileFullPath(const char *fileName)
+{
+
+}
+
+// Get file path relative to another path
+const char *micGetFileRelativePath(const char *fileName, const char *refPath)
+{
+
+}
+
+// Get file info: MIC_FILE_INFO_TIME_CREATION, MIC_FILE_INFO_TIME_LAST_ACCESS, MIC_FILE_INFO_TIME_LAST_WRITE
+long micGetFileInfo(const char *fileName, int info)
+{
+
+}
+
+// Get directory byte size (for all files contained)
+int micGetDirectorySize(const char *dirPath)
+{
+
+}
+
+// Get filenames in a directory path (memory should be freed)
+char **micGetDirectoryFiles(const char *dirPath, int *count)
+{
+
+}
+
+
+// String management (no UTF-8 strings, only byte chars)
+// NOTE: Some strings allocate memory internally for returned strings -> REVIEW
+//----------------------------------------------------------------------------------
+
+// Copy one string to another, returns bytes copied
+int micStringCopy(char *dstStr, const char *srcStr)
+{
+
+}
+
+// Check if two string are equal
+bool micStringEqual(const char *str1, const char *str2)
+{
+
+}
+
+// Get string size in bytes, checks for '\0' ending
+unsigned int micStringSize(const char *str)
+{
+
+}
+
+// String formatting with variables (sprintf() style)
+const char *micStringFormat(const char *str, ...)
+{
+
+}
+
+// Get a piece of a string
+const char *micStringSubstring(const char *str, int position, int length)
+{
+
+}
+
+// Replace string (WARNING: memory must be freed!)
+char *micStringReplace(char *str, const char *replace, const char *by)
+{
+
+}
+
+// Insert string in a position (WARNING: memory must be freed!)
+char *micStringInsert(const char *str, const char *insert, int position)
+{
+
+}
+
+// Join strings with delimiter
+const char *micStringJoin(const char **strList, int count, const char *delimiter)
+{
+
+}
+
+// Split string into multiple strings
+const char **micStringSplit(const char *str, char delimiter, int *count)
+{
+
+}
+
+// Append string at specific position and move cursor
+void micStringAppend(char *str, const char *append, int *position)
+{
+
+}
+
+// Find first string occurrence within a string
+int micStringFindIndex(const char *str, const char *find)
+{
+
+}
+
+// Get upper case version of provided string
+const char *micStringToUpper(const char *str)
+{
+
+}
+
+// Get lower case version of provided string
+const char *micStringToLower(const char *str)
+{
+
+}
+
+// Get Pascal case notation version of provided string
+const char *micStringToPascal(const char *str)
+{
+
+}
+
+// Get integer value from string (negative values not supported)
+int micStringToInteger(const char *str)
+{
+
+}
+
+// Check if a string contains another string
+bool micStringContains(const char *str, const char *contain)
+{
+
+}
+
+// Check if a string starts with another prefix string
+bool micStringStartsWith(const char *str, const char *start)
+{
+
+}
+
+// Misc functions
+//----------------------------------------------------------------------------------
+
+// Save integer value to storage file (to defined position), returns true on success
+bool micSaveStorageValue(unsigned int position, int value)
+{
+
+}
+
+// Load integer value from storage file (from defined position)
+int micLoadStorageValue(unsigned int position)
+{
+
+}
+
+// Get a random value between min and max (both included)
+int micGetRandomValue(int min, int max)
+{
+
+}
+
+// Set the seed for the random number generator
+void micSetRandomSeed(unsigned int seed)
+{
+
+}
+
+// Load file data as byte array (read)
+unsigned char *micLoadFileData(const char *fileName, unsigned int *bytesRead)
+{
+
+}
+
+// Unload file data allocated by LoadFileData()
+void micUnloadFileData(unsigned char *data)
+{
+
+}
+
+// Save data to file from byte array (write), returns true on success
+bool micSaveFileData(const char *fileName, void *data, unsigned int bytesToWrite)
+{
+
+}
+
+// Load text data from file (read), returns a '\0' terminated string
+char *micLoadFileText(const char *fileName)
+{
+
+}
+
+// Unload file text data allocated by LoadFileText()
+void micUnloadFileText(char *text)
+{
+
+}
+
+// Save text data to file (write), string must be '\0' terminated, returns true on success
+bool micSaveFileText(const char *fileName, char *text)
+{
+
+}
+
+// Compress file into a .zip
+int micZipFile(const char *srcFileName, const char *dstFileName)
+{
+
+}
+
+// Compress directory into a .zip
+int micZipDirectory(const char *srcPath, const char *dstFileName)
+{
+
+}
+
+// Compress data (DEFLATE algorithm)
+unsigned char *micCompressData(unsigned char *data, int dataLength, int *compDataLength)
+{
+
+}
+
+// Decompress data (DEFLATE algorithm)
+unsigned char *micDecompressData(unsigned char *compData, int compDataLength, int *dataLength)
+{
+
+}
 
 #endif   // MIC_IMPLEMENTATION
